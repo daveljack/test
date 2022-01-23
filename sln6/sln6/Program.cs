93ad4d99b645220639240d19afd1261fb99d6848
daveljack <98272128+daveljack@users.noreply.github.com>
2022-01-23 23:42:40 +0800

    Add files via upload

---
+++ sln6/sln6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/// <summary>
+/// 兩數總和
+/// </summary>
+namespace sln6
+{
+    class Program
+    {
+        public static int[] Two_Sum(int[] array,int target)
+        {
+            for(int i=0;i<array.Length;i++)
+            {
+                for(int j=i+1;j<array.Length;j++)
+                {
+                    if(array[i]+array[j]==target)
+                    {
+                        return new int[] {i,j};
+                    }
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int[] ans = Two_Sum( nums, 9);
+            Console.WriteLine(ans[0]+","+ans[1]);
+        }
+    }
+}
